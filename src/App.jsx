cf53ceb677726d3a13683f8a9cf2bda815fd12eb
Brian Hayes <bjoeyhayes@gmail.com>
2019-06-26 14:11:38 -0400

    Fix Error Boundary
    Everything navigates properly, and the modal can be closed.

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,18 @@ import './styles/_modal.scss';
 import './styles/foundation/main.scss';
 import styles from './App.module.scss';
 
-const mapStateToProps = state => ({
-  currentView: state.nav.currentView,
-  images: state.image.images,
-  selectedCollectionId: state.frame.selectedCollectionId,
-  currentBreakpoint: state.size.breakpoint,
-  appHeight: state.size.app.height,
-  appWidth: state.size.app.width,
-  isPortrait: state.size.app.isPortrait
-});
+const mapStateToProps = state => {
+  console.log(state);
+  return {
+    currentView: state.nav.currentView,
+    images: state.image.images,
+    selectedCollectionId: state.frame.selectedCollectionId,
+    currentBreakpoint: state.size.breakpoint,
+    appHeight: state.size.app.height,
+    appWidth: state.size.app.width,
+    isPortrait: state.size.app.isPortrait
+  };
+};
 
 class App extends Component {
   state = {
@@ -57,6 +60,7 @@ class App extends Component {
 
   render() {
     const { currentView, appHeight, appWidth } = this.props;
+    console.log(currentView);
     const { showWarning } = this.state;
     const style = {};
     const warningStyle = {
--- src/components/ErrorBoundary/ErrorBoundary.jsx
+++ src/components/ErrorBoundary/ErrorBoundary.jsx
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Button, Colors } from 'react-foundation';
 import { updateView } from '../../redux/actions/nav';
-import { Views } from '../../globals';
+import { Views, AppAtts } from '../../globals';
+import { closeModal } from '../../utils/CropShopButton';
 
 import styles from './ErrorBoundary.module.scss';
+
+const mapStateToProps = state => ({
+  currentView: state.nav.currentView,
+  previousView: state.nav.previousView
+});
 
 class ErrorBoundary extends Component {
   state = {
@@ -18,30 +24,53 @@ class ErrorBoundary extends Component {
     return { hasError: true, error, info };
   }
 
+  handleGoBack = () => {
+    const { currentView, previousView, dispatch } = this.props;
+    console.log('From Boundary\n', currentView, previousView);
+    this.setState({ hasError: false });
+
+    if (previousView) {
+      dispatch(updateView(previousView));
+    } else {
+      dispatch(updateView(currentView));
+    }
+  };
+
+  handleCloseModal = () => {
+    this.setState({ hasError: false });
+    closeModal(AppAtts.MODAL_ID);
+  };
+
   componentDidCatch(error, info) {
     // You can also log the error to an error reporting service
     console.log(error, info);
   }
 
   render() {
-    const { children, dispatch } = this.props;
+    const { children } = this.props;
     const { hasError } = this.state;
 
     if (hasError) {
       // You can render any custom fallback UI
       return (
         <div className={styles.container}>
-          <h1>Something went wrong.</h1>
+          <div className={styles.closeModal}>
+            <button
+              type='button'
+              className={styles.button}
+              aria-label='Close modal'
+              onClick={() => this.handleCloseModal()}
+            />
+          </div>
+
+          <h1>Something Went Wrong</h1>
           <p>
             Sorry about that, please click below to go back to the gallery and
             try again.
           </p>
 
-          <Button
-            color={Colors.PRIMARY}
-            onClick={() => dispatch(updateView(Views.GALLERY))}
-          >
-            Go Back to the Gallery
+          <Button color={Colors.PRIMARY} onClick={() => this.handleGoBack()}>
+            Go Back
           </Button>
         </div>
       );
@@ -51,4 +80,4 @@ class ErrorBoundary extends Component {
   }
 }
 
-export default connect()(ErrorBoundary);
+export default connect(mapStateToProps)(ErrorBoundary);
