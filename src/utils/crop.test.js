caa013bc888a94e14639f1f3050b73cee47d235d
Brian Hayes <bjoeyhayes@gmail.com>
2019-08-10 16:20:26 -0400

    add unit tests for crop utils

---
+++ src/utils/crop.test.js
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2019-present, Brian J. Hayes.
+ *
+ * https://github.com/joeyred
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { aspectRatioFill, getPosition, calcCropFullCentered } from './crop';
+
+describe('aspectRatioFill', () => {
+  it('Should maintain original aspect ratio of source', () => {
+    const source = {
+      width: 3580,
+      height: 2385
+    };
+    const max = {
+      width: 813.33,
+      height: 801.7
+    };
+
+    const sourceAspectRatio = source.width / source.height;
+    const maxAspectRatio = max.width / max.height;
+
+    const fill = aspectRatioFill(
+      source.width,
+      source.height,
+      max.width,
+      max.height
+    );
+
+    expect(fill.width / fill.height).toBe(sourceAspectRatio);
+  });
+});
